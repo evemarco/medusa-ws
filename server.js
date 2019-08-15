90ea95b5e4263b163e7bc8434a965a1d7a5f5e8b
evemarco <eve.icefr@gmail.com>
2019-08-15 17:30:47 +0200

    Fleet_id associated to name, log connection / deconnection, number of connected

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var io = require('socket.io').listen(5000)
  
 io.sockets.on('connection', (socket) => {
-  socket.on('name', (name) => {
-    socket.name = name
-    console.log(`${name} connected`)
+  socket.on('name', (result) => {
+    socket.name = result.name
+    socket.fleet_id = result.fleet_id
+    console.log(`${socket.name} connected / modified with fleet ${socket.fleet_id}`, '- NB connected:', io.sockets.server.eio.clientsCount)
   })
   socket.on('boss', (fleet) => {
     let fleetID = `fleet-${fleet.fleet_id}`
@@ -12,9 +13,9 @@ io.sockets.on('connection', (socket) => {
   })
 
   socket.on('msg', () => {
-    console.log('Chat message by ', socket.name);
+    console.log('Chat message by ', socket.name)
   })
   socket.on('disconnect', () => {
-    console.log(`${socket.name} disconnected`);
+    console.log(`${socket.name} disconnected`, '- NB connected:', io.sockets.server.eio.clientsCount)
   });
 })
